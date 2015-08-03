0f6b302d6757edf50c58ce0319847e53941fcc76
Pierre Souchay <pierre.souchay@cloudwatt.com>
2015-08-03 12:24:35 +0200

    Added limited email validation

--- api/src/main/java/com/cloudwatt/apis/bss/impl/CWIdentityQuery.java
+++ api/src/main/java/com/cloudwatt/apis/bss/impl/CWIdentityQuery.java
@@ -16,10 +16,15 @@ public class CWIdentityQuery implements FindUserQuery {
      * @param email
      * @param firstName
      * @param lastName
+     * @throws IllegalArgumentException if email is null or empty
      */
-    public CWIdentityQuery(String email, Optional<String> firstName, Optional<String> lastName) {
+    public CWIdentityQuery(String email, Optional<String> firstName, Optional<String> lastName)
+            throws IllegalArgumentException {
         if (email == null || email.trim().isEmpty()) {
-            throw new IllegalArgumentException("email cannot be null");
+            throw new IllegalArgumentException("email cannot be null or empty"); //$NON-NLS-1$
+        }
+        if (email.indexOf("@") < 1) { //$NON-NLS-1$
+            throw new IllegalArgumentException("email '" + email + "' is not valid"); //$NON-NLS-1$ //$NON-NLS-2$
         }
         this.firstName = firstName;
         this.lastName = lastName;
